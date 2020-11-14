ffc63e31a62a7a7eb878dae308430c172fc803bd
YILMAZ ALPEREN <74132936+yilmzalpren@users.noreply.github.com>
2020-11-14 20:10:07 +0300

    Update CoordinateGeo.java

--- CoordinateGeo.java
+++ CoordinateGeo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg1306190030_yilmaz_alperen;
+package CoordinateGeometry;
 import java.util.Scanner ; 
 import java.lang.Math; 
 /**
